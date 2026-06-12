92242a729edbb4135e25c065d1703f164f9fb52c
Jason <jason.ye914@gmail.com>
2026-06-12 17:46:38 -0400

    First commit

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import react from 'react'; 
+
+const signIn = () => { 
+    return (
+        <div>Sign In</div>
+    )
+}
+
+export default signIn; 
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import react from 'react'; 
+
+const signUp = () => {
+    return (
+        <div>Sign Up</div>
+    )
+}
+
+export default signUp; 
