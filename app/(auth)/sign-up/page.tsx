9ab0df1338ff4bbfa4ba9c81dbf111e1febbc5a3
Jason <jason.ye914@gmail.com>
2026-07-14 23:03:46 -0400

    [Done]: Sign in page functional

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,6 +1,9 @@
 import AuthForm from "@/components/ui/AuthForm"
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-const signUp = () => { 
+const signUp = async() => { 
+    const loggedInUser = await getLoggedInUser(); 
+    console.log(loggedInUser); 
     return (
         <section className="flex-center size-full max-sm:px-6">
             <AuthForm 
